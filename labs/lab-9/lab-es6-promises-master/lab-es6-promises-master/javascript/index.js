75eba7c776a7c053980e16aae583092e305f6112
santicasalis <santicasalis@hotmail.com>
2022-11-25 11:01:39 +0100

    lab

--- labs/lab-9/lab-es6-promises-master/lab-es6-promises-master/javascript/index.js
+++ labs/lab-9/lab-es6-promises-master/lab-es6-promises-master/javascript/index.js
@@ -27,14 +27,115 @@
 
 
 
-// Iteration 1 - using callbacks
-// ...
-
-// Iteration 2 - using promises
-// ...
+  obtainInstruction("steak", 0)
+  .then((paso) => {
+    document.querySelector("#steak").innerHTML += `<li>${paso}</li>`;
+    return obtainInstruction("steak", 1);
+  })
+  .then((paso) => {
+    document.querySelector("#steak").innerHTML += `<li>${paso}</li>`;
+    return obtainInstruction("steak", 2);
+  })
+  .then((paso) => {
+    document.querySelector("#steak").innerHTML += `<li>${paso}</li>`;
+    return obtainInstruction("steak", 3);
+  })
+  .then((paso) => {
+    document.querySelector("#steak").innerHTML += `<li>${paso}</li>`;
+    return obtainInstruction("steak", 4);
+  })
+  .then((paso) => {
+    document.querySelector("#steak").innerHTML += `<li>${paso}</li>`;
+    return obtainInstruction("steak", 5);
+  })
+  .then((paso) => {
+    document.querySelector("#steak").innerHTML += `<li>${paso}</li>`;
+    return obtainInstruction("steak", 6);
+  })
+  .then((paso) => {
+    document.querySelector("#steak").innerHTML += `<li>${paso}</li>`;
+    return obtainInstruction("steak", 7);
+  })
+  .then((paso) => {
+    document.querySelector("#steak").innerHTML += `<li>${paso}</li>`;
+    document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`;
+    document.querySelector("#steakImg").removeAttribute("hidden");
+  });
 
 // Iteration 3 using async/await
-// ...
+
+async function makeBroccoli() {
+ 
+  const step0 = await obtainInstruction("broccoli", 0);
+  document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`;
+  
+  const step1 = await obtainInstruction("broccoli", 1);
+  document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
+  
+  const step2 = await obtainInstruction("broccoli", 2);
+  document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
+ 
+  const step3 = await obtainInstruction("broccoli", 3);
+  document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+ 
+  const step4 = await obtainInstruction("broccoli", 4);
+  document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+  
+  const step5 = await obtainInstruction("broccoli", 5);
+  document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
+ 
+  const step6 = await obtainInstruction("broccoli", 6);
+  document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
+  
+  document.querySelector(
+    "#broccoli"
+  ).innerHTML += `<li>Broccoli is ready!</li>`;
+
+  document.querySelector("#broccoliImg").removeAttribute("hidden");
+}
+makeBroccoli();
+
 
 // Bonus 2 - Promise all
-// ...
+const paso0 = obtainInstruction("brusselsSprouts", 0);
+const paso1 = obtainInstruction("brusselsSprouts", 1);
+const paso2 = obtainInstruction("brusselsSprouts", 2);
+const paso3 = obtainInstruction("brusselsSprouts", 3);
+const paso4 = obtainInstruction("brusselsSprouts", 4);
+const paso5 = obtainInstruction("brusselsSprouts", 5);
+const paso6 = obtainInstruction("brusselsSprouts", 6);
+const paso7 = obtainInstruction("brusselsSprouts", 7);
+
+
+// ASYNC AWAIT
+async function makeBrusselsSprouts() {
+  
+  try {
+   
+    const valores = await Promise.all([
+      paso0,
+      paso1,
+      paso2,
+      paso3,
+      paso4,
+      paso5,
+      paso6,
+      paso7,
+    ]);
+
+    valores.forEach((instruccionDePaso) => {
+      document.querySelector(
+        "#brusselsSprouts"
+      ).innerHTML += `<li>${instruccionDePaso}</li>`;
+    });
+   
+    document.querySelector(
+      "#brusselsSprouts"
+    ).innerHTML += `<li>Yo yo yo my homie them brussels sprouts be ready man, time to eat some grub!</li>`;
+    
+    document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
+  } catch (error) {
+    console.log(error);
+  }
+}
+makeBrusselsSprouts();
